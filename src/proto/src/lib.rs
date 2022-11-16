470547e73de1df1f977f31b0c955ac9a73396902
Jason <madamsummer@163.com>
2022-11-16 14:53:49 +0800

    1. modify workflow; 2. add tarpaulin cfg to ignore code coverage

--- src/proto/src/lib.rs
+++ src/proto/src/lib.rs
@@ -1,10 +1,14 @@
 #[cfg(feature = "proto-common")]
+#[cfg(not(tarpaulin))]
 pub mod common;
 #[cfg(feature = "coordinator")]
+#[cfg(not(tarpaulin))]
 pub mod coordinator;
 
 #[cfg(feature = "worker")]
+#[cfg(not(tarpaulin))]
 pub mod worker;
 
 #[cfg(feature = "apiserver")]
+#[cfg(not(tarpaulin))]
 pub mod apiserver;
